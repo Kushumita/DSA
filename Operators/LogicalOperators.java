d166195c976675d1ede1e0f111204fa2e400e786
Kushumita <kushumitamahato5464@gmail.com>
2026-02-26 20:40:54 +0530

    Updated Operators

---
+++ Operators/LogicalOperators.java
@@ -0,0 +1,19 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        // Logical AND (&&)
+        System.out.println((a < b) && (b < c)); // true
+        System.out.println((a > b) && (b < c)); // false
+
+        // Logical OR (||)
+        System.out.println((a < b) || (b > c)); // true
+        System.out.println((a > b) || (b > c)); // false
+
+        // Logical NOT (!)
+        System.out.println(!(a < b)); // false
+        System.out.println(!(a > b)); // true
+    }
+}
