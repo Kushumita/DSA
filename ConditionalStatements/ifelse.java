c4b2fafca254d41a5d192b7ebb2f22e1dbf29c89
Kushumita <kushumitamahato5464@gmail.com>
2026-02-26 21:01:39 +0530

    Added ConditionalStatements

---
+++ ConditionalStatements/ifelse.java
@@ -0,0 +1,15 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int age=16;
+        if(age>=18){
+            System.out.println("Adult: Drive, Vote");
+        }
+        if(age>=13 && age<18){
+            System.out.println("Teenager");
+        }
+        else{
+            System.out.println("Not adult");
+        }	
+    }
+}
+
---
+++ ConditionalStatements/largestOfTwo.java
@@ -0,0 +1,12 @@
+public class largestOfTwo {
+    public static void main(String[] args) {
+        int a = 10;        
+        int b = 20;
+        if (a > b) {
+            System.out.println(a + " is the largest number.");
+        }
+        else {
+            System.out.println(b + " is the largest number.");
+        }
+    }  
+}
