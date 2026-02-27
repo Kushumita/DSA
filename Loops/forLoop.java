fbee100553a5ec9729bad06e8efc0a4be20feccd
Kushumita <kushumitamahato5464@gmail.com>
2026-02-27 20:31:59 +0530

    Updated Loops

---
+++ Loops/forLoop.java
@@ -0,0 +1,8 @@
+public class forLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Hello World");
+        }
+        System.out.println("Printed Hello World 100 times");
+    }    
+}
