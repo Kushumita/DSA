88d07d0c9c321f26cc795d71df9f128c7e6a622a
Kushumita <kushumitamahato5464@gmail.com>
2026-02-27 20:18:15 +0530

    Added Loops

---
+++ Loops/whileLoop.java
@@ -0,0 +1,10 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println("Hello World");
+            i++;
+        }
+        System.out.println("Printed Hello World 100 times");
+    }
+}
