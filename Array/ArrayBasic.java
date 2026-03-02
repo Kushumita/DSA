7b224c57e633f61069b24e44a28e4862677424bd
Kushumita <kushumitamahato5464@gmail.com>
2026-03-02 21:20:06 +0530

    Added Arrays

---
+++ Array/ArrayBasic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ArrayBasic {
+    public static void main(String[] args) {
+        int marks[]=new int[100];
+        Scanner sc=new Scanner(System.in);
+
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=95;
+        System.out.println("Physics: " + marks[0]);
+        System.out.println("Chemistry: " + marks[1]);
+        System.out.println("Maths: " + marks[2]);
+        
+        int percentage=(marks[0]+marks[1]+marks[2])/3;
+        System.out.println("Percentage: " + percentage);
+    }
+}
