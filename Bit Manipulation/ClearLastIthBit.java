30b0f7648019ef7ba1fd4fd8325c1da5a43a8999
Kushumita <kushumitamahato5464@gmail.com>
2026-03-06 20:05:50 +0530

    Updated Bit Manipulation

---
+++ Bit Manipulation/ClearLastIthBit.java
@@ -0,0 +1,9 @@
+public class ClearLastIthBit {
+    public static int clearLastithBit(int n,int i){
+        int bitMask=(~0)<<i;       
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(clearLastithBit(15,2));
+    }
+}
