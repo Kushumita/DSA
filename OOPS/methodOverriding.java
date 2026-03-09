d111eddbc8b464ba206073d748a3f6161977d4b3
Kushumita <kushumitamahato5464@gmail.com>
2026-03-09 21:11:16 +0530

    Updated OOPS

---
+++ OOPS/methodOverriding.java
@@ -0,0 +1,17 @@
+public class methodOverriding {
+    public static void main(String args[]){
+        Deer d=new Deer();
+        d.eat();
+    }
+}
+class Animal{
+    void eat(){
+        System.out.println("Eats Anything");
+    }   
+}
+
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eats Grass");
+    }
+}
