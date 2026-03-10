1a5ef52f206a60bdf97f5da42a06acc2a0b04ada
Kushumita <kushumitamahato5464@gmail.com>
2026-03-10 22:25:26 +0530

    Updated OOPS

--- OOPS/abstractClass.java
+++ OOPS/abstractClass.java
@@ -1,15 +1,16 @@
 public class abstractClass {
     public static void main(String[] args) {
-        Horse h=new Horse();
-        h.eat();
-        h.walk();
-        Chicken c=new Chicken();
-        c.eat();
-        c.walk();
+        Mustang myHorse=new Mustang();
+        //Animal -> Horse -> Mustang
     }
 }
 
 abstract class Animal{
+    String color;
+
+    Animal(){
+        System.out.println("Animal Constructor called");
+    }
     void eat(){
         System.out.println("Eats");
     }
@@ -17,12 +18,26 @@ abstract class Animal{
 }
 
 class Horse extends Animal{
+    Horse(){
+        System.out.println("Horse Constructor called");
+    }
+    void changeColor(){
+        color="dark brown";
+    }
     void walk(){
         System.out.println("Walks on 4 legs");
     }
 }
 
+class Mustang extends Horse{
+    Mustang(){
+        System.out.println("Mustang Constructor called");
+    }
+}
 class Chicken extends Animal{
+    void changeColor(){
+        color="yellow";
+    }
     void walk(){
         System.out.println("Walks on 2 legs");
     }
