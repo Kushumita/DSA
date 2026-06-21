f641be2c07e0d17764436dbbc133fdd57ed3d785
Kushumita <kushumitamahato5464@gmail.com>
2026-06-21 21:36:23 +0530

    Updated LL

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -35,6 +35,25 @@ public class LinkedList{
         //step3: tail=newNode
         tail=newNode;
     }
+    public void add(int idx,int data){
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        //step1: create new node
+        Node newNode=new Node(data);
+        //step2: find prev and curr
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+        //step3: newNode next=curr
+        newNode.next=temp.next;
+        //step4: prev next=newNode
+        temp.next=newNode;
+    }
     public void print(){
         Node temp=head;
         if(head==null){
@@ -50,12 +69,10 @@ public class LinkedList{
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
         ll.addFirst(2);
-        ll.print();
         ll.addFirst(1);
-        ll.print();
         ll.addLast(3);
-        ll.print();
         ll.addLast(4);
+        ll.add(2,9);
         ll.print();
     }
 }
