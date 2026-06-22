e66a3136a571a3f91b38e1690c038667eaf62297
Kushumita <kushumitamahato5464@gmail.com>
2026-06-22 22:14:04 +0530

    Updated LL

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -125,6 +125,18 @@ public class LinkedList{
         }
         return idx+1;
     }
+    public static boolean isCycle(){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next; //+1
+            fast=fast.next.next; //+2
+            if(slow==fast){
+                return true; //cycle exists
+            }
+        }
+        return false; //cycle does not exist
+    }
     public void print(){
         Node temp=head;
         if(head==null){
@@ -151,5 +163,11 @@ public class LinkedList{
         ll.print();
         System.out.println(ll.recSearch(3));
         System.out.println(ll.recSearch(10));
+        head=new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);
+        head.next.next.next=head;
+        //1->2->3
+        System.out.println(isCycle());
     }
 }
